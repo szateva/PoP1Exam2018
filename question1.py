b0f034d09ad5bd54eccb4731e1a0cb47bd8f10b1
Eva Szatmari <szateva@gmail.com>
2021-05-05 14:09:35 +0100

    question1.py added

---
+++ question1.py
@@ -0,0 +1,10 @@
+""" (a) In a method inside a class, what important value is held in the variable self? 2 marks
+Provide an appropriate example to illustrate you answer.
+(b) Inside a function foo you see the statement print(bar). 3 marks
+How do you determine whether bar is a local or global item?
+Provide a rule, or rules, for determining this.
+(c) Consider the following method (dened in a class WordPlay): 4 marks
+def isVowel(self, ch):
+return ch in "aeiouAEIOU"
+Write a complete PyTest test method for the isVowel method, testing positive
+and negative cases. """
